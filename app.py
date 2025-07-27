24b1e860c63d799dc7b6057dd456ef831c8998c3
Shogo1811 <shogo.n.1118@gmail.com>
2025-07-27 11:01:53 +0900

    コードリファクタリング

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, request, render_template, redirect, url_for
-from model_utils import process_and_predict, parse_distance
+from model_utils import process_and_predict
 import os
-import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
 
 # グローバル変数（予測結果保存用）
 rmse = None
@@ -33,58 +31,6 @@ def upload_file():
             return redirect(url_for("result_overview"))
 
     return render_template("upload.html")
-
-
-# # TODO 予測ページ動作検証中のため、コメントアウト
-# # 個別入力による予測ページ
-# @app.route("/predict_input", methods=["GET", "POST"])
-# def predict_input():
-#     predicted_price = None
-#     if request.method == "POST":
-#         area = request.form["area"]
-#         size = float(request.form["size"])
-#         age = int(request.form["age"])
-#         distance = int(request.form["distance"])
-
-#         current_year = 2025
-#         build_year = current_year - age
-
-#         input_df = pd.DataFrame([{
-#             "面積（㎡）": size,
-#             "最寄駅：距離（分）": distance,
-#             "建築年": build_year,
-#             "市区町村名": area
-#         }])
-
-#         # データ読み込みと整形
-#         df = pd.read_csv("./input/Tokyo_20242_20242.csv", encoding="cp932")
-#         df["建築年"] = pd.to_numeric(df["建築年"].str.replace("年", ""), errors="coerce")
-#         df["最寄駅：距離（分）"] = df["最寄駅：距離（分）"].apply(parse_distance)
-
-#         df = df.dropna()
-#         df = pd.get_dummies(
-#             df,
-#             columns=["市区町村名", "都道府県名", "種類", "価格情報区分", "間取り", "建物の構造", "用途", "都市計画"],
-#             drop_first=True
-#         )
-#         df = df.drop(columns=["地区名", "最寄駅：名称", "今後の利用目的", "改装", "取引の事情等", "取引時期"])
-
-#         all_columns = df.drop("取引価格（総額）", axis=1).columns
-#         input_df = pd.get_dummies(input_df, columns=["市区町村名"])
-#         for col in all_columns:
-#             if col not in input_df.columns:
-#                 input_df[col] = 0
-#         input_df = input_df[all_columns]
-
-#         # モデル学習と予測
-#         X = df.drop("取引価格（総額）", axis=1)
-#         y = df["取引価格（総額）"]
-#         model = RandomForestRegressor(random_state=42)
-#         model.fit(X, y)
-#         predicted_price = model.predict(input_df)[0]
-
-#     return render_template("predict_input.html", predicted_price=predicted_price)
-
 
 # 結果概要ページ（RMSEやスコア・リンク）
 @app.route("/result_overview")
